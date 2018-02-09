d9b6eb03801cd0976b90a952f53130d4c2d9e59d
msgdm <35688528+msgdm@users.noreply.github.com>
2018-02-09 16:06:13 +0530

    Create string.c

---
+++ string.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+    char a;
+    printf("enter the value\n");
+    scanf("%s",&"a");
+    if("a"==1&&1000)
+    {
+    printf("yes\n");
+    }
+    else
+    {
+    printf("no\n");
+    }
+}
