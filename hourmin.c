15cdbcd89474a255b0d9f3798dd9f5c12186ab7e
msgdm <35688528+msgdm@users.noreply.github.com>
2018-02-09 16:22:12 +0530

    Create hourmin.c

---
+++ hourmin.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+    int a,b,c,d,e,f;
+    printf("enter the hour and minutes\n");
+    scanf("%d%d",&a,&b);
+     printf("enter the hour and minutes\n");
+    scanf("%d%d",&c,&d);
+    e=a-c;
+    f=b-d;
+    printf("result %d %d",e,f);
+}
