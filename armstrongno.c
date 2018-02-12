c978963bc430fc4a39c495ebf3e5d528a92ce383
msgdm <35688528+msgdm@users.noreply.github.com>
2018-02-12 14:19:23 +0530

    Create armstrongno.c

---
+++ armstrongno.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d=0;
+    printf("Enter a three digit integer: ");
+    scanf("%d", &a);
+    b= a;
+    while (b != 0)
+    {
+       c =b%10;
+        d +=c*c*c;
+        b /= 10;
+    }
+    if(d ==a)
+        printf("%d is an Armstrong number.",a);
+    else
+        printf("%d is not an Armstrong number.",a);
+    return 0;
+}
