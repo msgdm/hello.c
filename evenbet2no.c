f839e81a3b23cab7e256ceacdf0ab1d0ce7dbe6f
msgdm <35688528+msgdm@users.noreply.github.com>
2018-02-12 14:12:55 +0530

    Create evenbet2no.c

---
+++ evenbet2no.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+main()
+{
+    int z,a,b;
+    printf("enter the limit\n");
+    scanf("%d%d",&a,&b);
+    printf("Print Odd Numbers in a given range a to b:\n");
+    for (z=a;z<=b;z++)
+        {
+          if (z%2 == 0)
+          printf ("%d ",z);
+         }
+}
