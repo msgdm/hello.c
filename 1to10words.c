024a6e6f8e99fe62b529e60c57021e3e3d1525fc
msgdm <35688528+msgdm@users.noreply.github.com>
2018-02-23 15:51:49 +0530

    Create 1to10words.c

---
+++ 1to10words.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+main()
+{
+    int a;
+    printf("enter the number\n");
+    scanf("%d",&a);
+    if(a==1)
+    printf("one\n");
+    else if(a==2)
+    printf("two");
+     else if(a==10)
+    printf("ten");
+     else if(a==3)
+    printf("three");
+     else if(a==4)
+    printf("four");
+     else if(a==5)
+    printf("five");
+     else if(a==6)
+    printf("six");
+     else if(a==7)
+    printf("seven");
+     else if(a==8)
+    printf("eight");
+     else if(a==9)
+    printf("nine");
+    
+}
