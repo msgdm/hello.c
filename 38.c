f5be05bb2e96b7aa31a7b978636e746518620e61
msgdm <35688528+msgdm@users.noreply.github.com>
2018-02-28 14:38:49 +0530

    Create 38.c

---
+++ 38.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void swap(int a, int b)
+{
+    int c = a;
+    a = b;
+    b = c;
+}
+ 
+int main()
+{
+    int q, z;
+    printf("Enter Value of q ");
+    scanf("%d", &q);
+    printf("\nEnter Value of z ");
+    scanf("%d", &z);
+    swap(&q, &z);
+    printf("\nAfter Swapping: q = %d, z = %d", z,q);
+    return 0;
+}
+© 2018 GitHub, Inc.
