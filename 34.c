0ddffbda42bbd9534fb04d89f9afd13786c5f81d
msgdm <35688528+msgdm@users.noreply.github.com>
2018-03-02 16:12:59 +0530

    Create 34.c

---
+++ 34.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+main()
+{
+   char s[100];
+   int l,count=1,i;
+   scanf("%s,s");
+   l=strlen(s);
+   for(i=0;i<l;i++)
+   {
+       if(s[i]=='.')
+       count++;
+       printf("%d",count);
+       else
+       printf("%d",count);
+   }
+
+    return 0;
+}
