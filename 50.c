638e469499dff1961d2d8fcd9e4d45afdb4ad34b
msgdm <35688528+msgdm@users.noreply.github.com>
2018-03-05 03:29:33 -0500

    Create 50.c

---
+++ 50.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("enter the numbers");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+    printf("no");
+    }
+    return 0;
+}
