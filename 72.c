1b9467fed453d5d114926a891444edde7b6e1302
msgdm <35688528+msgdm@users.noreply.github.com>
2018-03-09 16:38:14 +0530

    Create 72.c

---
+++ 72.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+    char a,b={"a","e","i","o","u"},i;
+    printf("enter the string\n");
+    scanf("%s",&a);
+    
+       
+    if(a==b)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+    
+}
