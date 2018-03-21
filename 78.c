d10e67c9f9194191be0b9bea7875760131d076a3
msgdm <35688528+msgdm@users.noreply.github.com>
2018-03-21 15:57:19 +0530

    Create 78.c

---
+++ 78.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+main() 
+{
+    int n,i;
+    scanf("%d",&n);
+    if(n%13==0)
+    {
+        printf("yes...%d is a multiple of 13",n);
+    }
+    else
+    {
+    printf("no");
+    }
+}
