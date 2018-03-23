f3ba872800a256ff65a68dab0fb0693bbc1c0c44
msgdm <35688528+msgdm@users.noreply.github.com>
2018-03-23 16:14:19 +0530

    Create 81.c

---
+++ 81.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+main()
+{
+    int a,b,c,d,e,f,g,h,i;
+    
+    printf("enter the kabali team numbers\n" "enter the opponant teammembers\n");
+    scanf("%d %d",&a,&b);
+    if(a>b)
+    {
+      g=a-b;
+    }
+     
+     printf("enter the kabali team numbers\n" "enter the opponant teammembers\n");
+    scanf("%d %d",&c,&d);
+    if(c>d)
+     {
+      h=c-d;
+     }
+     
+     printf("enter the kabali team numbers\n" "enter the opponant teammembers\n");
+    scanf("%d %d",&e,&f);
+    if(e>f)
+     {
+     i=e-f;
+     }
+    printf("%d%d%d",g,h,i);
+}
